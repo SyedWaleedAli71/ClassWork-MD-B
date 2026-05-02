40584797efa168c192e084e4a7f6151814d9a6e8
SyedWaleedAli71 <syedwaleed887@gmail.com>
2026-05-02 20:14:09 +0500

    Ready to commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// echo "# ClassWork-MD-B" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/SyedWaleedAli71/ClassWork-MD-B.git
+// git push -u origin main
+// 
+// git remote add origin https://github.com/SyedWaleedAli71/ClassWork-MD-B.git
+// git branch -M main
+// git push -u origin main
+// 
+
+
+// for(i=0;i<10;i++){
+// console.log(i);
+// }
+
+// 
+// var i = 0;
+// while(i < 20){
+//     console.log(i);
+//     document.writeln(i);
+//     i++;
+// }
+
+// 
+// var x = 10;
+// var x = 20;
+// console.log(x);
+
+//
+// let x = 10;
+// let x = 20;
+// console.log(x);
+
+// 
+// const arr = ["Syed","Waleed","Ali","Aaraiz","Ansari"];
+// arr [2]=10;
+// console.log(arr);
+
+// 
+// x = 10;
+// function my(){
+//     console.log(x);
+// }
+// my();
+
+// 
+// let x = `pakistan 
+// india 
+// iran`;
+// console.log(x);
+
+// 
+// let x = `He's Father is "Great" man.`;
+// console.log(x);
+
+// 
+// let x = `Jawan Pakistan `;
+// x = `Class`;
+// let s = ` Hello ${x}`;
+// console.log(s);
+
+// 
+const x = `"Syed Waleed Ali.."`;
+
+const NewX  = x.replace("Waleed","Zain");
+// const NewX = x.slice(0, 5) + " Zain" + x.slice(12);
+
+const s = `My name is ${NewX}`
+console.log(s);

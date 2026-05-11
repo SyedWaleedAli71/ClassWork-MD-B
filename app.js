17c3e49a58ab34530d618d75a77dcc9229df95da
SyedWaleedAli71 <syedwaleed887@gmail.com>
2026-05-11 05:02:55 +0500

    09-May-SaturDay

--- app.js
+++ app.js
@@ -63,10 +63,148 @@
 // console.log(s);
 
 // 
-const x = `"Syed Waleed Ali.."`;
+// const x = `"Syed Waleed Ali.."`;
 
-const NewX  = x.replace("Waleed","Zain");
-// const NewX = x.slice(0, 5) + " Zain" + x.slice(12);
+// const NewX  = x.replace("Waleed","Zain");
+// // const NewX = x.slice(0, 5) + " Zain" + x.slice(12);
 
-const s = `My name is ${NewX}`
-console.log(s);
+// const s = `My name is ${NewX}`
+// console.log(s);
+
+// 
+// **********Sat 9 may**************
+
+// let arr = [1,2,3];
+
+// let a =  arr[1];
+// console.log(arr[0]);
+
+// 
+
+// let arr = [1,2,3];
+
+// let [a,b,c] = arr;
+// console.log(a,b,c);
+
+// 
+
+// const color = ["Red","Green","Blue","Yellow"];
+
+// const [color1,,color2] = color;
+// console.log(color1 , color2);
+
+// 
+// let arr1 = [`Apple`,`cow`];
+// let [a,b = `Cake`] =arr1;
+// console.log(a,b);
+
+// 
+
+// let arr1 = [`Apple`,`cow`, `blue`];
+// let [a,b = `Cake`] =arr1;
+// console.log(a,b);
+
+// 
+// function zee(a,b,...restOp){
+
+//     console.log(a,b,restOp);
+    
+// }
+// zee("Car", "bike", "Boys")
+
+// 
+// let str = `J
+// a
+// w
+// a
+// n
+
+// P
+// a
+// k
+// i
+// s
+// t
+// a
+// n`;
+// console.log(str);
+
+// 
+// let str = 'Jawan Pakistan';
+// for (i of str){
+//     console.log(i);
+    
+// }
+// For of Loop ////////
+// let str = ['11'
+//     ,'22','33'];
+// for (i of str){
+//     console.log(i);
+    
+// }
+
+// 
+// let x = 10;
+// let y = 20;
+
+// [x,y]=[y,x]
+
+// console.log(x,y);
+// console.log(x);
+// console.log(y);
+
+// 
+// const nested = [1,[2,3]];
+
+// const [one,[two,three,]]= nested;
+
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(one,two,three);
+// console.log(nested);
+
+
+// 
+// const student = {
+//     name:  'Waleed',
+//     age: 23,
+//     city: "Karachi"
+// }
+// const { name, age, city} = student ;
+// console.log(student);
+// console.log(name,city);
+
+
+// 
+// const student = {
+//     name:  'Waleed',
+//     age: 23,
+//     city: "Karachi"
+// }
+// const { a, b} = student ;
+
+// console.log(a , b);
+
+// 
+// function print ({name,age}){
+//     console.log(`name${name} Age${age}`);
+    
+// }
+// const user = {name:" Ali", age: 20}
+// print(user);
+
+
+// 
+// let t =10;
+// let v =t;
+// v =30;
+// console.log(t,v);
+
+
+// 
+// let m = [ 1,2,3,4,5,6];
+// let sum = m.reduce((total,num)=> total + num);
+// console.log(sum);
+
+// 
